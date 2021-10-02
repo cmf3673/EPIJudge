7f78a3228a1922cfe0e6d9dfe68db0f9d5cac7a0
Caleb Ford <cmf3673@gmail.com>
2021-10-02 18:39:43 -0500

    Solved int_as_array_increment 5.2

--- epi_judge_python/int_as_array_increment.py
+++ epi_judge_python/int_as_array_increment.py
@@ -4,8 +4,25 @@ from test_framework import generic_test
 
 
 def plus_one(A: List[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    # SLOW for testing
+    # n = int(''.join([str(a) for a in A]))
+    # n += 1
+    # return [int(x) for x in str(n)]
+    if A[-1] != 9:
+        A[-1] += 1
+        return A
+    i = -1
+    n = len(A)
+    while A[i] == 9:
+        # if last element is 9
+        A[i] = 0
+        if i == -n:
+            A[0] = 1
+            A.append(0)
+            return A
+        i -= 1
+    A[i] += 1
+    return A
 
 
 if __name__ == '__main__':
