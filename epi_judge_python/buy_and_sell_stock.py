c69691cb7bc3fcca3da01390b6f48e511693e625
Caleb Ford <cmf3673@gmail.com>
2021-10-10 18:48:24 -0500

    Added stock solution

--- epi_judge_python/buy_and_sell_stock.py
+++ epi_judge_python/buy_and_sell_stock.py
@@ -1,11 +1,17 @@
+from math import inf
 from typing import List
 
 from test_framework import generic_test
 
-
+# Time: O(n)
+# Space: O(1)
 def buy_and_sell_stock_once(prices: List[float]) -> float:
-    # TODO - you fill in here.
-    return 0.0
+    max_p = float('-inf')
+    max_d = float('-inf')
+    for p in prices[::-1]:
+        if p > max_p: max_p = p
+        if max_p - p > max_d: max_d = max_p - p
+    return max_d
 
 
 if __name__ == '__main__':
