6a7ed3738eee8a83714c0ac6d0961e774a471cdc
Caleb Ford <cmf3673@gmail.com>
2021-10-12 01:21:57 -0500

    Added 6.1 solution

--- epi_judge_python/string_integer_interconversion.py
+++ epi_judge_python/string_integer_interconversion.py
@@ -1,15 +1,35 @@
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 
+# helper function for int_to_string
+def int_to_char(i):
+    return chr(i + ord('0'))
 
 def int_to_string(x: int) -> str:
-    # TODO - you fill in here.
-    return '0'
+    if x == 0:
+        return '0'
+    n = abs(x)
+    s = ''
+    while n > 0:
+        s += int_to_char(n % 10)
+        n //= 10
+    return ''.join(reversed(s)) if x >= 0 else '-' + ''.join(reversed(s))
 
+# helper for string_to_int
+def char_to_int(c):
+    return ord(c) - ord('0')
 
 def string_to_int(s: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    # n for place tracking
+    sum = n = 0
+    for c in reversed(s):
+        if c == '-':
+            return -sum
+        if c == '+':
+            return sum
+        sum += char_to_int(c) * (10 ** n)
+        n += 1
+    return sum
 
 
 def wrapper(x, s):
